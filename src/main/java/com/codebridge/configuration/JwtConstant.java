6fd45e6b63b9afa4f440a07f2ac9671a03a7ac16
Shivam Baranwal <shivambaranwal267@gmail.com>
2026-02-11 20:41:56 +0530

    first commit

---
+++ src/main/java/com/codebridge/configuration/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.codebridge.configuration;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "akjsjwoosnamqiepdbfgeu,ehdjwijsjs,quiwoqosknxgy,euubvbxmakwuesjishshshbmqisnsn";
+    public static final String JWT_HEADER = "Authorization";
+
+}
